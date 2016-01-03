3c3414b3115f86ec6c0c6ca1e5cef61b828a2bb9
francesc <ffores93@gmail.com>
2016-01-03 23:55:03 +0100

    migration and seed task/tags

--- database/migrations/2015_11_24_205051_create_tasks_table.php
+++ database/migrations/2015_11_24_205051_create_tasks_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,9 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('done');
+            $table->string('priority');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('tasks');
     }
 }
